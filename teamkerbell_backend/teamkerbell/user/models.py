abc19f8eb969b74b5e8e2873fb0cd6a426e99c76
yunseon99 <dldbstjs99@naver.com>
2024-05-18 14:28:47 +0900

    feat: 로그인 로그아웃 기능구현

--- teamkerbell_backend/teamkerbell/user/models.py
+++ teamkerbell_backend/teamkerbell/user/models.py
@@ -1,6 +1,7 @@
 # models.py
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class BasicUser(models.Model):
     id = models.AutoField(primary_key=True,null=False)
@@ -51,3 +52,33 @@ class Bookmark(models.Model):
 
 
 
+class MyLoginUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class LoginUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = MyLoginUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
