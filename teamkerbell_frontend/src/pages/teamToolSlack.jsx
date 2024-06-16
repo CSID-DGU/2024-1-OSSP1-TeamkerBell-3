17c81992a61da8a335ef1174edff61f6c95126a3
keaikelly <keaikelly@naver.com>
2024-06-16 23:38:33 +0900

    style: 가이드라인 설명 사진 추가 및 tid 관련 주소 수정

--- teamkerbell_frontend/src/pages/teamToolSlack.jsx
+++ teamkerbell_frontend/src/pages/teamToolSlack.jsx
@@ -6,14 +6,15 @@ import LeftSide from "../components/teamComponents/LeftSide";
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { listState } from "../atoms"; // recoil의 전역상태 listState 가져옴
+
 const TeamTool = () => {
   const listStateValue = useRecoilValue(listState); // Recoil 훅을 사용하여 상태 값 가져오기
 
   return (
     <div className={styles.container}>
-      {/*row로 정렬*/}
+      {/* row로 정렬 */}
       <div className={styles.left}>
-          <LeftSide />
+        <LeftSide />
       </div>
       <div className={styles.main}>
         {listStateValue === 0 ? <Slack /> : null}
@@ -23,13 +24,14 @@ const TeamTool = () => {
 };
 
 const Slack = () => {
+  const tid = localStorage.tid;
   return (
     <div className={styles.main2}>
       <div className={styles.title}>
         <h2>협업 툴 가이드라인</h2>
       </div>
       <hr className={styles.line} />
-      {/*title 정의*/}
+      {/* title 정의 */}
       <div className={styles.box}>
         <div className={styles.text}>
           <h1 className={styles.boxtitle}>Slack</h1>
@@ -41,10 +43,10 @@ const Slack = () => {
         </div>
       </div>
       <div>
-        <Link to={`/team/0/tools`}>
+        <Link to={`/team/${tid}/tools`} className={styles.link}>
           <button className={styles.backbutton}>목록</button>
         </Link>
-        {/*목록으로 돌아가는 버튼*/}
+        {/* 목록으로 돌아가는 버튼 */}
       </div>
     </div>
   );
@@ -54,21 +56,28 @@ const Content = () => {
   return (
     <div>
       {Title.map((Tcontent, Tindex) => (
-        <div key={Tindex}>
-          <h3 key={Tindex}>
-            {Tindex + 1}. {Tcontent}
+        <div key={Tindex} className={styles.section}>
+          <h3>
+            {Tindex + 1}. {Tcontent.title}
           </h3>
           {/* 제목 mapping 출력 */}
-
-          <ul>
-            {Descrip[Tindex].map((Dcontent, Dindex) => (
-              <li key={Dindex}>
-                {Dcontent} {/* 해당하는 Descrip의 요소를 출력 */}
-              </li>
-            ))}
-          </ul>
-
-          {/* 세부 mapping 출력 */}
+          <div className={styles.container}>
+            {Tcontent.image && (
+              <img
+                className={styles.img}
+                src={Tcontent.image}
+                alt={`Guide Image ${Tindex + 1}`}
+              />
+            )}
+            <ul className={styles.tRight}>
+              {Descrip[Tindex].map((Dcontent, Dindex) => (
+                <li key={Dindex}>
+                  {Dcontent} {/* 해당하는 Descrip의 요소를 출력 */}
+                </li>
+              ))}
+            </ul>
+            {/* 세부 mapping 출력 */}
+          </div>
         </div>
       ))}
     </div>
@@ -76,18 +85,29 @@ const Content = () => {
 };
 
 const Title = [
-  ["워크스페이스 생성"],
-  ["채널 생성 및 참여"],
-  ["다이렉트 메시지 및 그룹 메시지"],
-  ["파일 공유 및 통합"],
-  ["봇 및 앱 통합"],
+  {
+    image: "../../slackguide1.png",
+    title: "워크스페이스 생성 및 초대",
+  },
+  {
+    image: "../../slackguide2.png",
+    title: "채널 생성 및 참여",
+  },
+  {
+    image: "../../slackguide3.png",
+    title: "메시지 및 파일 공유",
+  },
+  {
+    image: "../../slackguide4.png",
+    title: "봇 및 앱 통합",
+  },
 ];
 
 const Descrip = [
   [
     ["Slack 웹사이트에 접속하여 새로운 워크스페이스를 생성합니다."],
     [
-      "워크스페이스 이름 및 URL, 초대할 팀원의 이메일 주소를 입력하여 워크스페이스를 생성합니다.",
+      "워크스페이스 이름 및 URL, 초대할 팀원의 이메일 주소를 입력하여 워크스페이스를 생성하고 팀원을 초대합니다.",
     ],
   ],
   [
@@ -96,18 +116,14 @@ const Descrip = [
     ],
     ["필요한 팀원을 해당 채널에 초대하여 참여할 수 있습니다."],
   ],
-
   [
     [
       "개별 팀원에게 다이렉트 메시지를 보내거나 특정 그룹을 만들어 메시지를 공유할 수 있습니다.",
     ],
-  ],
-  [
     [
       "파일 및 문서를 Slack에 업로드하여 팀원들과 공유하고, Google Drive, Dropbox 등 다른 서비스와 연동하여 파일을 공유할 수 있습니다.",
     ],
   ],
-
   [
     [
       "Slack 앱 스토어에서 다양한 봇 및 앱을 설치하여 업무 효율성을 높일 수 있습니다.",
--- teamkerbell_frontend/src/pages/teamToolW2M.jsx
+++ teamkerbell_frontend/src/pages/teamToolW2M.jsx
@@ -13,16 +13,15 @@ const TeamTool = () => {
     <div className={styles.container}>
       {/*row로 정렬*/}
       <div className={styles.left}>
-          <LeftSide />
+        <LeftSide />
       </div>
-      <div className={styles.main}>
-        {listStateValue === 0 ? <W2M /> : null}
-      </div>
+      <div className={styles.main}>{listStateValue === 0 ? <W2M /> : null}</div>
     </div>
   );
 };
 
 const W2M = () => {
+  const tid = localStorage.tid;
   return (
     <div className={styles.main2}>
       <div className={styles.title}>
@@ -43,7 +42,7 @@ const W2M = () => {
         </div>
       </div>
       <div>
-        <Link to={`/team/0/tools`} className={styles.link}>
+        <Link to={`/team/${tid}/tools`} className={styles.link}>
           <button className={styles.backbutton}>목록</button>
         </Link>
         {/*목록으로 돌아가는 버튼*/}
@@ -57,20 +56,27 @@ const Content = () => {
     <div>
       {Title.map((Tcontent, Tindex) => (
         <div key={Tindex}>
-          <h3 key={Tindex}>
-            {Tindex + 1}. {Tcontent}
+          <h3>
+            {Tindex + 1}. {Tcontent.title}
           </h3>
           {/* 제목 mapping 출력 */}
-
-          <ul>
-            {Descrip[Tindex].map((Dcontent, Dindex) => (
-              <li key={Dindex}>
-                {Dcontent} {/* 해당하는 Descrip의 요소를 출력 */}
-              </li>
-            ))}
-          </ul>
-
-          {/* 세부 mapping 출력 */}
+          <div className={styles.container}>
+            {Tcontent.image && (
+              <img
+                className={styles.img}
+                src={Tcontent.image}
+                alt={`Guide Image ${Tindex + 1}`}
+              />
+            )}
+            <ul className={styles.tRight}>
+              {Descrip[Tindex].map((Dcontent, Dindex) => (
+                <li key={Dindex}>
+                  {Dcontent} {/* 해당하는 Descrip의 요소를 출력 */}
+                </li>
+              ))}
+            </ul>
+            {/* 세부 mapping 출력 */}
+          </div>
         </div>
       ))}
     </div>
@@ -78,53 +84,47 @@ const Content = () => {
 };
 
 const Title = [
+  {
+    image: "../../w2mguide1.png",
+    title: "이벤트 생성",
+  },
+  {
+    image: "../../w2mguide2.png",
+    title: "링크 공유",
+  },
+  {
+    image: "../../w2mguide3.png",
+    title: "시간 선택 및 결과 확인",
+  },
+]; /*const Title = [
   ["이벤트 생성"],
-  ["이벤트 설정"],
   ["링크 공유"],
-  ["가능한 시간 선택"],
+  ["가능한 시간 선택 "],
   ["결과 확인 및 최적 시간 결정"],
-];
+];*/
 
 const Descrip = [
   [
-    ["When2Meet 홈페이지에 접속합니다."],
-    [
-      ' "Create a New Event" 섹션에서 이벤트의 이름과 자신의 이름을 입력합니다.',
-    ],
-    [
-      "날짜 범위를 선택합니다. 시작일과 종료일을 설정하여 해당 기간 동안의 가능한 모든 시간을 표시합니다.",
-    ],
+    ["상단 버튼 클릭을 통해 When2Meet 홈페이지에 접속합니다."],
+    ["이벤트의 이름을 입력하고 날짜 범위를 선택합니다. "],
     ['"Create Event" 버튼을 클릭하여 이벤트를 생성합니다.'],
   ],
   [
+    ["이벤트 생성이 완료되면, 고유한 이벤트 링크가 생성됩니다."],
     [
-      "이벤트를 생성한 후, 시간대를 확인하고 필요에 따라 조정합니다. When2Meet은 기본적으로 시스템의 시간대를 사용하지만, 이는 변경이 가능합니다.",
-    ],
-    ['"Save This Event" 버튼을 클릭하여 설정을 저장합니다.'],
-  ],
-
-  [
-    [
-      "이벤트 생성이 완료되면, 고유한 이벤트 링크가 생성됩니다. 이 링크를 참가자들과 공유하여 그들이 자신의 가능한 시간을 표시할 수 있게 합니다.",
+      "이 링크를 참가자들과 공유하여 팀원들이 자신의 가능한 시간을 표시할 수 있게 합니다.",
     ],
   ],
 
   [
-    ["참가자들은 공유받은 링크를 통해 이벤트 페이지에 접속합니다.     "],
+    ["참가자들은 공유받은 링크를 통해 이벤트 페이지에 접속합니다."],
     [
-      '참가자는 자신의 이름을 입력한 후, "Available" 섹션에서 가능한 시간을 드래그하여 선택합니다. 선택된 시간은 다른 참가자들에게도 보여집니다.',
+      "참가자는 자신의 이름을 입력한 후, 왼쪽 섹션에서 가능한 시간을 드래그하여 선택합니다. ",
     ],
     [
-      "참가자들은 필요에 따라 언제든지 자신의 가능한 시간을 업데이트할 수 있습니다. ",
+      "모든 참가자의 가능한 시간과 겹치는 시간을 오른쪽 섹션에서 확인할 수 있습니다. ",
     ],
-  ],
-  [
-    [
-      "이벤트 주최자와참가자들은 When2Meet 페이지에서 모든 참가자의 가능한 시간이 겹치는 부분을 확인할 수 있습니다. 이는 보통 녹색으로 표시됩니다. ",
-    ],
-    [
-      "가장 많은 사람들이 참여할 수 있는 시간대를 기준으로 모임이나 이벤트의 시간을 결정합니다.",
-    ],
+    ["가장 많은 사람들이 참여할 수 있는 시간대를 기준으로 결정합니다."],
   ],
 ];
 
