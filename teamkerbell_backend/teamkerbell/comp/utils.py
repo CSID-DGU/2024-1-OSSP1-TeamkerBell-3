68e077ec658ad3bbd955c7ba5cc707e72a75934e
yunseon99 <dldbstjs99@naver.com>
2024-06-18 22:56:09 +0900

    fix: 이메일 내용에서 홈페이지 주소 및 이미지 변경

--- teamkerbell_backend/teamkerbell/comp/utils.py
+++ teamkerbell_backend/teamkerbell/comp/utils.py
@@ -6,10 +6,7 @@ import requests
 from django.core.mail import EmailMultiAlternatives
 def send_team_matched_email(team_members, comp_name):
     # 이미지 다운로드
-    response = requests.get("https://teamkerbellbucket.s3.ap-northeast-2.amazonaws.com/teamkerbellLofo.png")
-    img_content = BytesIO(response.content).getvalue()
-    img_base64 = base64.b64encode(img_content).decode('utf-8')
-    
+
     for member in team_members:
         subject = '팀 매칭 완료'
         text_content = f'안녕하세요, {member.nickname}님!\n\n팀 매칭이 완료되었습니다. 이제 팀원들과 함께 공모전을 준비할 수 있습니다.\n\n감사합니다.'
@@ -22,7 +19,7 @@ def send_team_matched_email(team_members, comp_name):
                 <p>지금 링크를 클릭해 확인해 보세요!</p>
                 <p>https://d3lxmx9ope3ebd.cloudfront.net/</p>
                 <p>감사합니다.</p>
-                <img src="data:image/jpeg;base64,{img_base64}" alt="image" width=538 height=126>
+                <img src="https://teamkerbellbucket.s3.ap-northeast-2.amazonaws.com/formail.png" alt="image" width=744 height=485>
             </body>
         </html>
         '''
